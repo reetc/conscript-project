7f49f7b542d80af202c0a1ffb61681f12dfaae0e
Niti Shah <nits2097@gmail.com>
2018-10-21 00:47:03 +0530

    update company models

--- company/models.py
+++ company/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Company_details(models.Model):
+	company_name = models.CharField(max_length = 60)
+	company_email = models.CharField(max_length = 60)
+	company_id = models.IntegerField()
+	company_phn = models.CharField(max_length = 10)
+	company_city = models.CharField(max_length = 60)
+
+
+class Job_details(models.Model):
+	job_id = models.IntegerField()
+	job_name = models.CharField(max_length = 60)
+	job_location = models.CharField(max_length = 60)
+	job_position = models.CharField(max_length = 60)
+	company_details = models.ForeignKey('Company_details', related_name='jobs')
+
+class Question_details(models.Model):
+	question_name = models.CharField(max_length = 60)
+	question_model_answer = models.CharField(max_length = 100)
+	question_id = models.IntegerField()
+	question_type = models.CharField(max_length = 60)
+	job_details = models.ForeignKey('Job_details', related_name='questions')
+
