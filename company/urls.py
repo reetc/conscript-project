dc32dfe365306434297ef4aedf4025bc157075c1
Reet <reetchatterjee@gmail.com>
2018-10-23 02:36:04 +0530

    Company Integration

--- company/urls.py
+++ company/urls.py
@@ -6,6 +6,9 @@ from django.urls import path
 
 # Be careful setting the name to just /login use userlogin instead!
 urlpatterns=[
-    url(r'^candidate_list/$',views.candidate_list,name='candidate_list'),
+    url(r'^admin_dashboard/$',views.admin_dashboard,name='admin_dashboard'),
     url(r'^interview_list/$',views.interview_list,name='interview_list'),
+    url(r'^invite/$',views.invite,name='invite'),
+    url(r'^domains/$',views.domains,name='domains'),
 ]
+
--- company/views.py
+++ company/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-def candidate_list(request):
+def admin_dashboard(request):
 	return render(request, 'company/index.html')
 
 def interview_list(request):
 	return render(request, 'company/interviewList.html')
+
+def invite(request):
+    return render(request, 'company/invite.html')
+
+def domains(request):
+    return render(request, 'company/domains.html')
+
+
+
+
