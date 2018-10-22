9cbeb475ba3c29f83ff29443050a00b6922ebcaf
Vinay Varma <vinay.n.varma189@gmail.com>
2018-10-22 18:38:08 +0530

    following a single codestyle

--- conscript/urls.py
+++ conscript/urls.py
@@ -1,19 +1,4 @@
 
-from django.contrib import admin
-<<<<<<< HEAD
-# from django.urls import path
-from django.conf.urls import url,include
-from django.contrib import admin
-from company import views
-
-urlpatterns = [
-url(r'^$',views.index,name='index'),
-url(r'^admin/', admin.site.urls),
-url(r'^company/',include('company.urls')),
-url(r'^logout/$', views.user_logout, name='logout'),
-
-]
-=======
 from django.urls import path
 from django.conf.urls import include
 from django.conf import settings
@@ -22,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('candidate/', include('candidate.urls')),
+    path('company/', include('company.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
->>>>>>> ac98461a2ab4a74078ba9059834743969894db7b
