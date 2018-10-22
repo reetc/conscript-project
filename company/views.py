af38cb7c378bf6d819d2da52518f616f4e64390a
Vinay Varma <vinay.n.varma189@gmail.com>
2018-10-22 19:16:46 +0530

    connected homepage and company page

--- company/views.py
+++ company/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def candidate_list(request):
+	return render(request, 'company/index.html')
+
+def interview_list(request):
+	return render(request, 'company/interviewList.html')
