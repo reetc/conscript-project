9901ea39228b849f9d71658b723c7500c7acd884
Reet <reetchatterjee@gmail.com>
2018-10-23 23:19:59 +0530

    Integration of Domains(Job) Creation with the backend

--- company/models.py
+++ company/models.py
@@ -4,22 +4,30 @@ from django.db import models
 class Company_details(models.Model):
 	company_name = models.CharField(max_length = 60)
 	company_email = models.CharField(max_length = 60)
-	company_id = models.IntegerField()
+	# company_id = models.AutoField(primary_key=True,default=0)
 	company_phn = models.CharField(max_length = 10)
 	company_city = models.CharField(max_length = 60)
 
+	def __str__(self):
+		return "%s" %(self.company_name)
+
 
 class Job_details(models.Model):
-	job_id = models.IntegerField()
+	# job_id = models.AutoField(primary_key=True,default=0)
 	job_name = models.CharField(max_length = 60)
 	job_location = models.CharField(max_length = 60)
 	job_position = models.CharField(max_length = 60)
-	company_details = models.ForeignKey('Company_details', related_name='jobs',on_delete= models.CASCADE)
+	company = models.ForeignKey('Company_details', related_name='jobs',on_delete= models.CASCADE)
+
+	def __str__(self):
+		return "%s" %(self.job_name)
+
+
 
 class Question_details(models.Model):
 	question_name = models.CharField(max_length = 60)
-	question_model_answer = models.CharField(max_length = 100)
-	question_id = models.IntegerField()
+	model_answer = models.CharField(max_length = 100)
+	# question_id = models.AutoField(primary_key=True,default=0)
 	question_type = models.CharField(max_length = 60)
-	job_details = models.ForeignKey('Job_details', related_name='questions',on_delete= models.CASCADE)
-
+	job = models.ForeignKey('Job_details', related_name='questions',on_delete= models.CASCADE)
+	time_limit=models.IntegerField(default=3)
--- company/views.py
+++ company/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Job_details
+from .models import Company_details
+from .models import Question_details
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
 # Create your views here.
 def admin_dashboard(request):
 	return render(request, 'company/index.html')
@@ -11,8 +15,29 @@ def invite(request):
     return render(request, 'company/invite.html')
 
 def domains(request):
-    return render(request, 'company/domains.html')
+	if request.method == 'POST':
+
+		job=Job_details()
+		job.job_name = request.POST['job_name']
+		job.job_location = request.POST['job_location']
+		job.job_position = request.POST['job_position']
+		job.company=Company_details.objects.get(company_name="DEF")
+		job.save()
+
+		question=Question_details()
+		question.question_name = request.POST['question']
+		question.model_answer = request.POST['sample_answer']
+		question.question_type = request.POST['question_type']
+		question.time_limit = int(request.POST['time_limit'])
+		question.job = job
+		question.save()
 
 
 
-
+		previous_page = request.META['HTTP_REFERER']
+		data = {'previous_page': previous_page,'abc': 56}
+		return render(request,'company/domains.html',data)
+	else:
+		previous_page = request.META['HTTP_REFERER']
+		data = {'previous_page': previous_page,'abc': 56}
+		return render(request,'company/domains.html',data)
