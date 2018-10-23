b1407a18889abcbc5d7783276fd31102e78b953e
Reet <reetchatterjee@gmail.com>
2018-10-24 02:26:58 +0530

    Added time field in candidate side job_application model

--- candidate/models.py
+++ candidate/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from company.models import Job_details
+from django.utils.translation import gettext as _
+import datetime
 
 # Create your models here.
 class Candidate_personal_details(models.Model):
-	candidate_name = models.CharField(max_length = 60)
-	candidate_email = models.CharField(max_length = 60,default="")
-	# cadidate_id = models.AutoField(primary_key=True,default=0)
-	candidate_age = models.IntegerField()
+    candidate_name = models.CharField(max_length = 60)
+    candidate_email = models.CharField(max_length = 60,default="")
+    # cadidate_id = models.AutoField(primary_key=True,default=0)
+    candidate_age = models.IntegerField()
 
 class Job_application_details(models.Model):
-
-	# company_name = models.CharField(max_length = 60)
-	# job_location = models.CharField(max_length = 60)
-	# application_id=models.AutoField(primary_key=True,default=0)
-
-	job_id=models.ForeignKey(Job_details,on_delete= models.CASCADE,default=0)
-	# job_position = models.CharField(max_length = 60)
-	candidate_id_Foreign= models.ForeignKey(Candidate_personal_details, on_delete= models.CASCADE,default=0)
+    
+    # company_name = models.CharField(max_length = 60)
+    # job_location = models.CharField(max_length = 60)
+    # application_id=models.AutoField(primary_key=True,default=0)
+    
+    job_id=models.ForeignKey(Job_details,on_delete= models.CASCADE,default=0)
+    # job_position = models.CharField(max_length = 60)
+    candidate_id_Foreign= models.ForeignKey(Candidate_personal_details, on_delete= models.CASCADE,default=0)
+    date = models.DateField(_("Date"), default=datetime.date.today)
