8b5ec3c8f88d659e16ef160320f2c1c7df06c0c1
Vinay Varma <vinay.n.varma189@gmail.com>
2018-11-26 19:58:16 +0530

    added requirements1.txt

--- conscript/wsgi.py
+++ conscript/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conscript-project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conscript.settings')
 
 application = get_wsgi_application()
